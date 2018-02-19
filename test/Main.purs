1bfc8c3ad42ccc6ae2283b6fd07ff3c7c7f113df
Philippe Vinchon <pvinchon@gilt.com>
2018-02-19 17:07:00 +0000

    Test foreign aws sdk js requests

--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,43 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Aff (Aff, attempt, launchAff, throwError)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Console (log)
+import Control.Monad.Eff.Exception (error, message)
+import Data.Either (Either(..))
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
+import Aws.Service (AwsError, request)
+
+main = launchAff do
+    _ <- testRequestUnknownService
+    _ <- testRequestUnknownMethod
+    _ <- testRequestMissingParameters
+    liftEff $ log "You should add some tests."
+
+testRequestUnknownService :: forall eff. Aff (err :: AwsError | eff) Unit
+testRequestUnknownService = do
+    errOrSuccess <- attempt $ request "unknown" "" ""
+    case errOrSuccess of
+        Right succ -> throwError $ error "AWS service unknown shouldn't exist"
+        Left err -> if (message err) == "awsSdk[serviceName] is not a constructor"
+            then pure unit
+            else throwError err
+
+testRequestUnknownMethod :: forall eff. Aff (err :: AwsError | eff) Unit
+testRequestUnknownMethod = do
+    errOrSuccess <- attempt $ request "S3" "unknown" ""
+    case errOrSuccess of
+        Right succ -> throwError $ error "AWS S3 method unknown shouldn't exist"
+        Left err -> if (message err) == "awsService[methodName] is not a function"
+            then pure unit
+            else throwError err
+
+testRequestMissingParameters :: forall eff. Aff (err :: AwsError | eff) Unit
+testRequestMissingParameters = do
+    errOrSuccess <- attempt $ request "S3" "getBucketVersioning" ""
+    case errOrSuccess of
+        Right succ -> throwError $ error "AWS S3 getBucketVersioning should require parameters"
+        Left err -> if (message err) == "Missing required key 'Bucket' in params"
+            then pure unit
+            else throwError err
