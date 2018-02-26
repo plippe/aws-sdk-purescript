c845622d4cf887071b42e0322f51ded9926fe6fe
Philippe Vinchon <pvinchon@gilt.com>
2018-02-26 11:24:35 +0000

    Extracted aws request logic to own project

---
+++ aws/test/Main.purs
@@ -0,0 +1,47 @@
+module Test.Main where
+
+import Prelude
+import Control.Monad.Aff (Aff, attempt, launchAff, throwError)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Console (log)
+import Control.Monad.Eff.Exception (error, message)
+import Data.Either (Either(..))
+
+import Aws.Service
+
+main = launchAff do
+    _ <- testRequestUnknownService
+    _ <- liftEff $ log "OK. testRequestUnknownService"
+    _ <- testRequestUnknownMethod
+    _ <- liftEff $ log "OK. testRequestUnknownMethod"
+    _ <- testRequestMissingParameters
+    _ <- liftEff $ log "OK. testRequestMissingParameters"
+
+    pure unit
+
+testRequestUnknownService :: forall eff. Aff (err :: AwsError | eff) Unit
+testRequestUnknownService = do
+    errOrSuccess <- attempt $ request "unknown" "" ""
+    case errOrSuccess of
+        Right succ -> throwError $ error "AWS service unknown shouldn't exist"
+        Left err -> if (message err) == "awsSdk[serviceName] is not a constructor"
+            then pure unit
+            else throwError err
+
+testRequestUnknownMethod :: forall eff. Aff (err :: AwsError | eff) Unit
+testRequestUnknownMethod = do
+    errOrSuccess <- attempt $ request "S3" "unknown" ""
+    case errOrSuccess of
+        Right succ -> throwError $ error "AWS S3 method unknown shouldn't exist"
+        Left err -> if (message err) == "awsService[methodName] is not a function"
+            then pure unit
+            else throwError err
+
+testRequestMissingParameters :: forall eff. Aff (err :: AwsError | eff) Unit
+testRequestMissingParameters = do
+    errOrSuccess <- attempt $ request "S3" "getBucketVersioning" ""
+    case errOrSuccess of
+        Right succ -> throwError $ error "AWS S3 getBucketVersioning should require parameters"
+        Left err -> if (message err) == "Missing required key 'Bucket' in params"
+            then pure unit
+            else throwError err
