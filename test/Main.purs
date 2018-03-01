e3ca31fecb6bc495411f0a39bf23f3305035b3bb
Philippe Vinchon <pvinchon@gilt.com>
2018-03-01 11:55:04 +0000

    Fix tests

--- test/Main.purs
+++ test/Main.purs
@@ -5,12 +5,14 @@ import Control.Monad.Aff (Aff, Fiber, attempt, launchAff, throwError)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Control.Monad.Eff.Exception (error, message)
+import Control.Monad.Eff.Exception (EXCEPTION, Error, error, message)
 import Data.Either (Either(..))
+import Data.Foreign (Foreign)
 
-import AWS.Request (RequestError, request)
+import AWS.Request (request)
+import AWS.Request.Types (NoInput(..))
 
-main :: forall eff. Eff (err :: RequestError, console :: CONSOLE | eff) (Fiber (err :: RequestError, console :: CONSOLE | eff) Unit)
+main :: forall eff. Eff (exception :: EXCEPTION, console :: CONSOLE | eff) (Fiber (exception :: EXCEPTION, console :: CONSOLE | eff) Unit)
 main = launchAff do
     _ <- testRequestUnknownService
     _ <- liftEff $ log "OK. testRequestUnknownService"
@@ -21,27 +23,27 @@ main = launchAff do
 
     pure unit
 
-testRequestUnknownService :: forall eff. Aff (err :: RequestError | eff) Unit
+testRequestUnknownService :: forall eff. Aff (exception :: EXCEPTION | eff) Unit
 testRequestUnknownService = do
-    errOrSuccess <- attempt $ request "unknown" "" ""
+    errOrSuccess :: Either Error Foreign <- attempt $ request "unknown" "" (NoInput unit)
     case errOrSuccess of
         Right succ -> throwError $ error "AWS service unknown shouldn't exist"
         Left err -> if (message err) == "awsSdk[serviceName] is not a constructor"
             then pure unit
             else throwError err
 
-testRequestUnknownMethod :: forall eff. Aff (err :: RequestError | eff) Unit
+testRequestUnknownMethod :: forall eff. Aff (exception :: EXCEPTION | eff) Unit
 testRequestUnknownMethod = do
-    errOrSuccess <- attempt $ request "S3" "unknown" ""
+    errOrSuccess :: Either Error Foreign <- attempt $ request "S3" "unknown" (NoInput unit)
     case errOrSuccess of
         Right succ -> throwError $ error "AWS S3 method unknown shouldn't exist"
         Left err -> if (message err) == "awsService[methodName] is not a function"
             then pure unit
             else throwError err
 
-testRequestMissingParameters :: forall eff. Aff (err :: RequestError | eff) Unit
+testRequestMissingParameters :: forall eff. Aff (exception :: EXCEPTION | eff) Unit
 testRequestMissingParameters = do
-    errOrSuccess <- attempt $ request "S3" "getBucketVersioning" ""
+    errOrSuccess :: Either Error Foreign <- attempt $ request "S3" "getBucketVersioning" (NoInput unit)
     case errOrSuccess of
         Right succ -> throwError $ error "AWS S3 getBucketVersioning should require parameters"
         Left err -> if (message err) == "Missing required key 'Bucket' in params"
